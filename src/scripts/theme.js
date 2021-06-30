d8d0431b994bd8d5cef7c4f18698e5e83b4d68da
Youness <std.youness@gmail.com>
2021-06-30 23:32:56 +0200

    Added class.

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -3,8 +3,11 @@ var foo = 'Youness'
 console.log(foo)
 console.log(foo)
 console.log(foo)
-console.log(foo)
-console.log(foo)
-console.log(foo)
-console.log(foo)
-console.log(foo)
+
+class Input {
+  constructor() {
+    console.log('Hi mate.')
+  }
+}
+
+new Input()
