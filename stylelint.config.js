857833294818ae93943d77ad6ba11184f78d8dc8
Youness <std.youness@gmail.com>
2021-07-06 13:33:43 +0200

    added an announcement bar.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-sass-guidelines', 'stylelint-config-prettier'],
+  rules: {
+    'selector-class-pattern':
+      '^[a-z]([a-z0-9-]+)?(_([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$|^Mui.*$',
+  },
 }
