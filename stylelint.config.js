a793c55a990aa1f081b0b649c755a80877eac13b
Youness <std.youness@gmail.com>
2021-07-08 10:50:23 +0200

    Modified the styles folder architecture.

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['stylelint-config-sass-guidelines', 'stylelint-config-prettier'],
   rules: {
     'selector-class-pattern':
-      '^[a-z]([a-z0-9-]+)?(_([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$|^Mui.*$',
+      '-^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
   },
 }
